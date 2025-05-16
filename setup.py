49f5d5caf4b1d34bbf422a05d98795b7d74c9725
chingis <chingis@tripla.jp>
2025-05-16 09:26:55 +0900

    start project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shrunkiq",
+    version="0.1.0",
+    author="Chingis Owana",
+    author_email="chingisoinar@gmail.com",
+    description="ShrunkIQ Project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cowana-ai/ShrunkIQ",
+    license="MIT",
+    python_requires=">=3.9,<3.12",
+    packages=find_packages(),
+) 
